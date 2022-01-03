4893061b238a8b4eb7a3a67b51f920ade3f90eb9
Jacob D <jwdonaldson99@gmail.com>
2022-01-03 16:05:07 -0700

    Add project files.

---
+++ FirstWebApp413/wwwroot/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿//Javascript file --Jacob Donaldson
+
+
+$("#btnsend").click(function () {
+    alert("From: " + $("#name").val()  +
+        "\nEmail: " + $("#email").val()  +
+        "\nQuestions: " + $("#questions").val());
+
+    $("#picanderson").fadeOut("slow");
+})
